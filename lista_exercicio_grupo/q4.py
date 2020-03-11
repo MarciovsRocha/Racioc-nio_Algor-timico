8ede71c19e00331e4adc4c8bfbf9cdb061e0ab33
MarciovsRocha <dev.marcio.rocha@gmail.com>
2020-03-11 11:12:20 -0300

    otimizado o exercicio 4

--- lista_exercicio_grupo/q4.py
+++ lista_exercicio_grupo/q4.py
@@ -7,17 +7,17 @@ def calcIMC(p,a: float):
 def comparaIMC(imc: float):
     if (imc < 17.0):
         print(f"Sua situacao eh: Muito abaixo do peso, IMC = {imc}")
-    elif (17.0 <= imc < 18.5):
+    elif (imc < 18.5):
         print(f"Sua situacao eh: Abaixo do peso, IMC = {imc}")
-    elif (18.5 <= imc <25.0):
+    elif (imc <25.0):
         print(f"Sua situacao eh: Peso normal, IMC = {imc}")
-    elif (25.0 <= imc < 30.0):
+    elif (imc < 30.0):
         print(f"Sua situacao eh: Acima do peso, IMC = {imc}")
-    elif (30.0 <= imc < 35.0):
+    elif (imc < 35.0):
         print(f"Sua situacao eh: Obesidade I, IMC = {imc}")
-    elif (35.0 <= imc < 40.0):
+    elif (imc < 40.0):
         print(f"Sua situacao eh: Obesidade II (severa), IMC = {imc}")
-    elif (imc > 40.0):
+    elif (imc >= 40.0):
         print(f"Sua situacao eh Obesidade III (mórbida), IMC = {imc}")
 
 a = float(input("Digite a sua altura: "))
