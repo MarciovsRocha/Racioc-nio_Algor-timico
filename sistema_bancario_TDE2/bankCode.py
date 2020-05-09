558ec8dfb7e17434e63efbdd06f9b256434ba725
MarciovsRocha <dev.marcio.rocha@gmail.com>
2020-05-09 16:28:59 -0300

    realizado procedimento de busca e de troca de senha de contas

--- sistema_bancario_TDE2/bankCode.py
+++ sistema_bancario_TDE2/bankCode.py
@@ -2,7 +2,7 @@ import json
 import sys
 import os
 from gerente import *
-from cliente import * 
+from cliente import *
 verificado = False
 
 # usuario logado
@@ -16,12 +16,14 @@ userLoged = {}
 
 clear = lambda: os.system('cls')
 
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 # funcao para ler dados do arquivo JSON
 def loadJSONData():
     with open('./data/data.json') as json_file:
         data = json.load(json_file)
         return data
 
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 # funcao para fazer login
 def login(verificado, user, password, data):
     if (verificado == True):
@@ -32,12 +34,14 @@ def login(verificado, user, password, data):
             verificado = True
             return data['conta'][i]
 
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 def geraIDNovo(data):
     pos = len(data['conta'])
     ultimoId = data['conta'][pos-1]['id']
     idNovo = ultimoId+1
     return idNovo
 
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 def selectMenu(loggedUser):
     if ((loggedUser['type'] == 'G') or (loggedUser['type'] == 'g')):
         escolha = Gmenu()
@@ -48,17 +52,32 @@ def selectMenu(loggedUser):
             salva = input('Deseja Salvar as alteracoes feitas? [S]im [N]ao\n')
             if ((salva == 's') or (salva == 'S')):
                 salvaData(newuser)
+        elif (escolha == 2):
+            srcNome = input('Digite o Nome a ser buscado:\n')
+            data = loadJSONData()
+            result = buscaConta(srcNome,data)
+            print(f'Resultado para a busca: {srcNome}')
+            for i in range(len(result)):
+                print(f'id: {result[i]["id"]} -- nome: {result[i]["nome"]} {result[i]["sobrenome"]}\n ')
+        elif(escolha == 3):
+            idConta = int(input('Digite o ID da Conta cuja senha sera troca: '))
+            senhaNova = input('Digite a senha para o cliente de 4 a 8 digitos \n caracteres aceitos: a-z, A-Z,0-9 \n Senha:')
+            data = loadJSONData()
+            newData = trocaSenhaConta(idConta,senhaNova,data)
+            # criar procedimento para alterar a data
+            with open('./data/data.json', 'w') as outfile:
+                json.dump(newData,outfile)
     elif ((loggedUser['type'] == 'C') or (loggedUser['type'] == 'c')):
         escolha = Cmenu()
 
-
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 # funcao para salvar o arquivo json
 def salvaData(object):
     jsonFile = json.loads(open('./data/data.json').read())
     jsonFile['conta'].append(object)
     with open('./data/data.json', 'w') as outfile:
         outfile.write(json.dumps(jsonFile))
-
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 # funcao para que o usuario faca login
 def forcaLogin(verificado):
     # variavel para contar numero de tentavas e se estourar 
@@ -83,7 +102,7 @@ def forcaLogin(verificado):
         else:
             verificado = True
             return userLoged, verificado
-
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 while True:
     if (verificado == True):
         clear()
--- sistema_bancario_TDE2/gerente.py
+++ sistema_bancario_TDE2/gerente.py
@@ -7,11 +7,27 @@ def Gmenu():
     return choice
 
 def cadUser(id):
-    nomeCliente = input('Digite o Nome completo do cliente:\n')
+    nomeCliente = input('Digite o Nome do cliente:\n')
+    sobreNome =input('Digite o sobrenome do cliente \n')
     profissao = input('Digite a Profissao do cliente\n')
     rendaMensal = float(input('Digite a renda mensal do cliente: (formato: ###.#)\n'))
     endereco = input('Digite o endereco do cliente:\n')
     telefone = input('Digite o telefone de contato do cliente:\n')
     senha = input('Digite a senha para o cliente de 4 a 8 digitos \n caracteres aceitos: a-z, A-Z,0-9 \n Senha:')
-    newUser = {"id": id,"password": senha,"nome": nomeCliente, "balance": 0.0,"type": "c", "endereco":  endereco, "telefone": telefone, "profissao": profissao, "rendaMensal": rendaMensal}
+    # colocar uma funcao para verificar a senha antes dele criar o objeto cliente
+    newUser = {"id": id,"password": senha,"nome": nomeCliente, "sobrenome": sobreNome, "balance": 0.0,"type": "c", "endereco":  endereco, "telefone": telefone, "profissao": profissao, "rendaMensal": rendaMensal}
     return newUser
+
+def buscaConta(nome, data):
+    len_clientes = len(data['conta'])
+    listaContas = []
+    for i in range(0,len_clientes):
+        if (nome == data['conta'][i]['nome']):
+            listaContas.append(data['conta'][i])
+    return listaContas
+
+def trocaSenhaConta(id, senhaNova, data):
+    for i in range(0,len(data['conta'])):
+        if (id == data['conta'][i]["id"]):
+            data['conta'][i]['password'] = senhaNova
+            return data
