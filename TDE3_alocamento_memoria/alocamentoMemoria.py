df779128b52919dce7f60f87ab89d699ba71fa65
MarciovsRocha <dev.marcio.rocha@gmail.com>
2020-05-31 18:45:26 -0300

    arrumar alocacao de memoria

--- TDE3_alocamento_memoria/alocamentoMemoria.py
+++ TDE3_alocamento_memoria/alocamentoMemoria.py
@@ -3,7 +3,7 @@ memoria = []
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
 # funcao para criar a 'memoria'
-def criaMatriz(Xmem,Ymem: int):
+def createMemory(Xmem,Ymem: int):
     if ((Xmem <= 0) or (Ymem <=0)):
         print('Os valores nao podem ser menores ou iguais a zero')
         return 0
@@ -17,21 +17,51 @@ def criaMatriz(Xmem,Ymem: int):
     while i < Xmem:
         memAloc.append(X)
         i += 1
-    showMemoria(memAloc)
+    return memAloc
 # ---------------------------------------------------------------------------------------------------------------------------------------
-# visualizacao da matriz
-def showMemoria(matriz):
+# funcao para printar a memoria e seus espacos alocados com X
+def showMemory(matriz):
     for i in range(0,len(matriz)):
+        print('|',end='')
         for j in range(0,len(matriz[i])):
             if (matriz[i][j] == True):
                 print(' X |', end='')
             else:
-                print(' |', end='')
-            #print(f'[{matriz[i][j]}]', end='')
+                print('   |', end='')
         print()
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
+# funcao para alocar valores na memoria
+def alocMemory(X,Y,memory):
+    if (len(X) != len(Y)):
+        print('Houve algum erro :( \n tente novamente mais tarde.')
+        return -1
+    for i in range(0,len(X)):
+        for j in range(0,len(Y)):
+            memory[i][j] = True
+    return memory
+# ---------------------------------------------------------------------------------------------------------------------------------------
 # first fit
+def firstFitAloc(aloc: int, memory):
+    count = 0
+    posX = []
+    posY = []
+    for i in range(0,len(memory)):
+        for j in range(0,len(memory)):
+            if (count == aloc):
+                memoria = alocMemory(posX,posY,memory)
+                return memoria
+            elif (memory[i][j] == True):
+                count = 0
+                posX.clear()
+                posY.clear()
+            elif (memory[i][j] == False):
+                count += 1
+                posX.append(i)
+                posY.append(j)
+                print(posX)
+                print(posY)
+                print(count)
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
 # best fit
@@ -46,4 +76,9 @@ print('Digite o tamanho da memoria (X,Y) \n sendo X = linhas e Y = colunas')
 qtdeLinhas = int(input('X: '))
 qtdeColunas = int(input('Y: '))
 print(f'criando uma memoria com tamanho({qtdeLinhas},{qtdeColunas})')
-criaMatriz(qtdeLinhas,qtdeColunas)
+memoria = createMemory(qtdeLinhas,qtdeColunas)
+showMemory(memoria)
+print()
+print()
+memoria = firstFitAloc(3,memoria)
+showMemory(memoria)
